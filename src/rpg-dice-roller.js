389db882587389a037acd685d1c9b37886548e47
Ricardo de Sousa Vieira <rickhard_22@hotmail.com>
2017-11-07 03:07:24 +0000

    Working

--- src/rpg-dice-roller.js
+++ src/rpg-dice-roller.js
@@ -1,61 +1,71 @@
 "use strict";
 
-function Dice(faceCount) {
-    if (faceCount === void 0) { faceCount = 6; }
-    this.faceCount = faceCount;
-}
-
 function Roller(value, faceCount, modifier) {
-    if (value === void 0) { value = 1; }
-    if (faceCount === void 0) { faceCount = 6; }
-    if (modifier === void 0) { modifier = 0; }
     this.validator = /\b([1-9]\d*)?d([1-9]\d*)?([-+]{1}[1-9]\d*)?\b/mi;
+
     if (typeof value === "string") {
         this.parseDiceNotation(value.trim().toLowerCase());
-    }
-    else {
-        this.createDices(Number(value), Number(faceCount));
-        this.modifier = Number(modifier);
+    } else {
+        this.setDiceCount(value).setFaceCount(faceCount).setModifier(modifier);
     }
 }
 
-Roller.prototype.createDices = function (amount, faceCount) {
-    this.dices = [];
-    while (this.dices.length < amount) {
-        this.dices.push(new Dice(faceCount));
-    }
+Roller.prototype.setDiceCount = function(val) {
+    this.diceCount = (!isNaN(parseFloat(val)) && isFinite(val) && (val % 1 === 0)) ? Number(val) : 1;
+    return this;
 };
 
-Roller.prototype.parseDiceNotation = function (value) {
+Roller.prototype.setFaceCount = function(val) {
+    this.faceCount = (!isNaN(parseFloat(val)) && isFinite(val) && (val % 1 === 0)) ? Number(val) : 6;
+    return this;
+};
+
+Roller.prototype.setModifier = function(val) {
+    this.modifier = (!isNaN(parseFloat(val)) && isFinite(val) && (val % 1 === 0)) ? Number(val) : 0;
+    return this;
+};
+
+Roller.prototype.parseDiceNotation = function(value) {
     if (!this.validator.test(value)) {
-        throw new Error("Malformed dicenotation");
+        throw new Error("Malformed dicenotation. Dicenotation format is like: ${diceCount}d${faceCount}${modifier}");
     }
-    var groups = this.validator.exec(value);
-    console.log("group#", groups)
-    this.createDices(Number(groups[1]), Number(groups[2]));
-    this.modifier = Number(groups[3]);
+
+    const groups = this.validator.exec(value);
+    this.setDiceCount(groups[1]).setFaceCount(groups[2]).setModifier(groups[3]);
 };
 
-Roller.prototype.roll = function () {
-    console.log("this#", this)
-    return this.dices.length * (Math.floor(this.dices[0].faceCount * Math.random()) + 1) + this.modifier;
-};
+Roller.prototype.roll = function() {
+    return this.diceCount * (Math.floor(this.faceCount * Math.random()) + 1) + this.modifier;
+}; 
 
-Roller.prototype.toDiceNotation = function () {
-    return this.dices.length + "d" + this.dices[0].faceCount + this.modifier;
+Roller.prototype.toDiceNotation = function() {
+    const dicenotation = this.diceCount + "d" + this.faceCount;
+    if (this.modifier) {
+        dicenotation += this.modifier;
+    }
+
+    return dicenotation;
 };
 
 function roll() {
-    var roller = arguments.length === 1 ? new Roller(arguments[0]) : new Roller(arguments[0], arguments[1], arguments[2]);
+    const roller = arguments.length === 1 ? new Roller(arguments[0]) : new Roller(arguments[0], arguments[1], arguments[2]);
     return roller.roll();
 }
 
 function createRoller() {
-    var roller = arguments.length === 1 ? new Roller(arguments[0]) : new Roller(arguments[0], arguments[1], arguments[2]);
-    Object.defineProperty(roller.roll, "toDiceNotation", {
-        value: roller.toDiceNotation
+    const roller = arguments.length === 1 ? new Roller(arguments[0]) : new Roller(arguments[0], arguments[1], arguments[2]);
+
+    function roll() {
+        return roller.roll();
+    }
+
+    Object.defineProperty(roll, "toDiceNotation", {
+        value: function() {
+            return roller.toDiceNotation();
+        }
     });
-    return roller.roll;
+
+    return roll;
 }
 
-module.exports = { roll: roll, createRoller: createRoller };
+module.exports = { roll, createRoller };
--- test/dice-roll-test.js
+++ test/dice-roll-test.js
@@ -11,27 +11,71 @@ describe('Import module:', function () {
 
 describe('Function "roll"', function () {
     const roll = require('../src/rpg-dice-roller').roll;
-    
-    it('"roll" returns a number: whole, random & according to arguments', function () {
+
+    it('Calling "roll" returns a number: whole, random & according to arguments', function () {
         for (let i = 0; i < 5; ++i) {
             let diceCount = Math.floor(Math.random() * 8) + 1;
             let faceCount = Math.floor(Math.random() * 120) + 1;
             let modifier = Math.floor(Math.random() * (diceCount * faceCount + 1)) + (diceCount * faceCount / 2);
 
-            expect(roll(diceCount, faceCount)).to.be.a('number').above(diceCount - 1).and.below(diceCount * faceCount + modifier + 1);
-        } 
+            expect(roll(diceCount, faceCount, modifier)).to.be.a('number').above(diceCount - 1).and.below(diceCount * faceCount + modifier + 1);
+        }
 
         expect(roll("3d7-2")).to.be.a('number').above(2).and.below(3 * 7 + (-2) + 1);
-        expect(roll("3d7-2") % 1).to.be.equal(0)
+        expect(roll("3d7-2") % 1).to.be.equal(0);
 
         expect(roll("1d6+1")).to.be.a('number').above(1).and.below(1 * 6 + (+1) + 1);
-        expect(roll("1d6+1") % 1).to.be.equal(0)
+        expect(roll("1d6+1") % 1).to.be.equal(0);
 
         expect(roll("4d")).to.be.a('number').above(3).and.below(4 * 6 + (0) + 1);
-        expect(roll("4d") % 1).to.be.equal(0)
+        expect(roll("4d") % 1).to.be.equal(0);
 
         expect(roll("d10")).to.be.a('number').above(0).and.below(1 * 10 + (0) + 1);
-        expect(roll("d10") % 1).to.be.equal(0)
+        expect(roll("d10") % 1).to.be.equal(0);
+
+    });
+
+});
+
+describe('Function "createRoller"', function () {
+    const createRoller = require('../src/rpg-dice-roller').createRoller;
+
+    it('"createRoller" returns a function with property toDiceNotation', function () {
+        const roller = createRoller();
+        expect(roller).to.have.property('toDiceNotation');
+    });
+
+    it('Calling "createRoller" returns a number: whole, random & according to arguments', function () {
+        for (let i = 0; i < 5; ++i) {
+            let diceCount = Math.floor(Math.random() * 8) + 1;
+            let faceCount = Math.floor(Math.random() * 120) + 1;
+            let modifier = Math.floor(Math.random() * (diceCount * faceCount + 1)) + (diceCount * faceCount / 2);
+
+            expect(createRoller(diceCount, faceCount, modifier)()).to.be.a('number').above(diceCount - 1).and.below(diceCount * faceCount + modifier + 1);
+        }
+
+        expect(createRoller("3d7-2")()).to.be.a('number').above(2).and.below(3 * 7 + (-2) + 1);
+        expect(createRoller("3d7-2")() % 1).to.be.equal(0);
+
+        expect(createRoller("1d6+1")()).to.be.a('number').above(1).and.below(1 * 6 + (+1) + 1);
+        expect(createRoller("1d6+1")() % 1).to.be.equal(0);
+
+        expect(createRoller("4d")()).to.be.a('number').above(3).and.below(4 * 6 + (0) + 1);
+        expect(createRoller("4d")() % 1).to.be.equal(0);
+
+        expect(createRoller("d10")()).to.be.a('number').above(0).and.below(1 * 10 + (0) + 1);
+        expect(createRoller("d10")() % 1).to.be.equal(0);
+
+    });
+
+    it('Calling "createRoller" with weird input', function () {
+        const roller = createRoller();
+
+        expect(createRoller(3, 7, -2)()).to.be.a('number').above(2).and.below(3 * 7 + (-2) + 1);
+        expect(createRoller(3, 7, -2)() % 1).to.be.equal(0);
+
+        expect(createRoller(3, 7, -2)()).to.be.a('number').above(2).and.below(3 * 7 + (-2) + 1);
+        expect(createRoller(3, 7, -2)() % 1).to.be.equal(0);
 
     });
 
